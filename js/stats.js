0512c083c6e5688d5760f830d3abad0edc8e36d6
BENCH <bench@local>
2026-08-23 22:11:40 +1000

    bench: before write js/ui/log.js

--- js/stats.js
+++ js/stats.js
@@ -158,7 +158,7 @@ export function hourDial(drives) {
   const peak = Math.max(1, ...hours.map(h => h.ms));
   return hours.map((h, i) => ({
     hour: i,
-    minutes: fmtMins(h.ms),
+    minutes: formatMinutes(h.ms),
     frac: h.ms / peak,
     cat: h.sent > h.chose ? 'sent' : 'chose'
   }));
